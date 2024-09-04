e2f5a320fd57b48a8898de0939f00cbe1cbeb790
FPetronilho <jfgp_10@live.com.pt>
2024-09-04 11:36:10 +0200

    Implemented data provider interfaces for Expense and ExpenseCategory. Method findAll() on Expense data provider will have to be revisited to take into account much more parameters other than offset and limit.

---
+++ expense-tracker-core/src/main/java/com/portfolio/expense_tracker/dataprovider/ExpenseDataProvider.java
@@ -0,0 +1,26 @@
+package com.portfolio.expense_tracker.dataprovider;
+
+import com.portfolio.expense_tracker.domain.Expense;
+import com.portfolio.expense_tracker.dto.ExpenseCreate;
+import com.portfolio.expense_tracker.dto.ExpenseUpdate;
+
+import java.util.List;
+
+public interface ExpenseDataProvider {
+
+    Expense create(ExpenseCreate expenseCreate);
+
+    Expense findById(String id);
+
+    List<Expense> findAll(
+            Integer limit,
+            Integer Offset
+    );
+
+    Expense update(
+            String id,
+            ExpenseUpdate expenseUpdate
+    );
+
+    void delete(String id);
+}
