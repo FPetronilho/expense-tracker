a7469886b031ada0ef866da10e1868d55b70180f
FPetronilho <jfgp_10@live.com.pt>
2024-10-08 00:55:43 +0200

    Updated https certificates as these were not working in postman.

--- expense-tracker-core/src/main/java/com/portfolio/expensetracker/usecases/expense/DeleteUseCase.java
+++ expense-tracker-core/src/main/java/com/portfolio/expensetracker/usecases/expense/DeleteUseCase.java
@@ -33,7 +33,7 @@ public class DeleteUseCase {
         if (!AssetResponse.PermissionPolicy.OWNER.equals(asset.getPermissionPolicy())) {
             throw new AuthorizationFailedException(
                     String.format(
-                            "Digital user %s can not delete expense %s because it is not its owner",
+                            "Digital user %s cannot delete expense %s because it is not its owner",
                             user.getId(),
                             input.getId()
                     )
