3a90f25ead6a9a3d7fc4c65f8728e198b0c51c32
FPetronilho <jfgp_10@live.com.pt>
2024-10-07 21:16:59 +0200

    Removed validation from AssetRequest DTO.

--- expense-tracker-core/src/main/java/com/portfolio/expensetracker/dto/portfoliomanager/request/AssetRequest.java
+++ expense-tracker-core/src/main/java/com/portfolio/expensetracker/dto/portfoliomanager/request/AssetRequest.java
@@ -1,10 +1,7 @@
 package com.portfolio.expensetracker.dto.portfoliomanager.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
-import com.portfolio.expensetracker.util.Constants;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,18 +10,9 @@ import com.portfolio.expensetracker.util.Constants;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AssetRequest {
 
-    @NotNull(message = Constants.ASSET_EXTERNAL_ID_MANDATORY_MSG)
-    @Pattern(regexp = Constants.ID_REGEX, message = Constants.EXTERNAL_ID_INVALID_MSG)
     private String externalId;
-
-    @NotNull(message = Constants.ASSET_TYPE_MANDATORY_MSG)
-    @Pattern(regexp = Constants.TYPE_REGEX, message = Constants.TYPE_INVALID_MSG)
     private String type;
-
-    @NotNull(message = Constants.ASSET_ARTIFACT_INFO_MANDATORY_MSG)
     private ArtifactInformation artifactInfo;
-
-    @NotNull(message = Constants.ASSET_PERMISSION_POLICY_MANDATORY_MSG)
     private PermissionPolicy permissionPolicy;
 
     @ToString
@@ -45,16 +33,8 @@ public class AssetRequest {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ArtifactInformation {
 
-        @NotNull(message = Constants.ASSET_GROUP_ID_MANDATORY_MSG)
-        @Pattern(regexp = Constants.GROUP_ID_REGEX, message = Constants.GROUP_ID_INVALID_MSG)
         private String groupId;
-
-        @NotNull(message = Constants.ASSET_ARTIFACT_ID_MANDATORY_MSG)
-        @Pattern(regexp = Constants.ARTIFACT_ID_REGEX, message = Constants.ARTIFACT_ID_INVALID_MSG)
         private String artifactId;
-
-        @NotNull(message = Constants.ASSET_VERSION_MANDATORY_MSG)
-        @Pattern(regexp = Constants.VERSION_REGEX, message = Constants.VERSION_INVALID_MSG)
         private String version;
     }
 }
